70651ea48139c1b46b4a82d0e7181c1eb9c5a1f8
dirceupj <dirceuu@gmail.com>
2019-08-10 23:41:05 -0300

    send to GitHub

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
-# Ruby-Vips-NDVI-and-VARI
-Leverages libvips image processing library and their Ruby binding to apply NDVI (Normalized Difference Vegetation Index) and VARI (Visible Atmospherically Resistant Index) Vegeration Index in NIR (near-infrared) and RGB orthophotos...
+# Ruby-Vips NDVI and VARI
+
+Leverages **[libvips](https://libvips.github.io/libvips/)** image processing library and their [Ruby binding](https://github.com/libvips/ruby-vips) to apply NDVI (Normalized Difference Vegetation Index) and VARI (Visible Atmospherically Resistant Index) Vegeration Index in NIR (near-infrared) and RGB orthophotos.
+
+It is a simplified version of my project [tiled-vegetation-indices](https://github.com/dirceup/tiled-vegetation-indices/) where I demonstrate how to compute statistics from orthophoto thumbnails then use it to apply the index to [map tiles](https://en.wikipedia.org/wiki/Tiled_web_map).
+
+## Running it:
+
+Install [libvips](https://github.com/libvips/ruby-vips) and [Ruby](https://www.ruby-lang.org/en/). Then use Ruby's **gem** to install 'ruby-vips' and 'histogram' packages:
+
+```
+gem install histogram
+gem install ruby-vips
+```
+
+Okay? Call the program:
+
+```
+ruby ruby-vips-vari-ndvi.rb
+```
+
+_nir.png_ and _rgb.png_ orthophoto thumbnails will be processed and resulting _ndvi.png_ and _vari.png_ will be saved.
---
+++ ruby-vips-vari-ndvi.rb
@@ -0,0 +1,109 @@
+# install with 'gem install histogram'
+require 'histogram/array'
+
+# install with 'gem install ruby-vips'
+require 'ruby-vips'
+
+# it is bundled with...
+require './colormaps.rb'
+include ColorMaps
+
+
+# find min/max and histogram
+def result_histogram(result)
+  flat_result = result.to_a.flatten
+  flat_result.reject! &:nan?
+  flat_result.histogram(256)[1]
+end
+
+# Remaps value (that has an expected range of in_low to in_high) into a target range of to_low to to_high
+def math_map_value(value, in_low, in_high, to_low, to_high)
+  to_low + (value - in_low) * (to_high - to_low) / (in_high - in_low)
+end
+
+def find_clipped_min_max(histogram, min, max)
+  # histogram
+  sum = histogram.inject(:+)
+  three_percent = sum * 0.03
+  lower_sum = 0
+  upper_sum = 0
+  last_lower_i = nil
+  last_upper_i = nil
+
+  histogram.each_with_index do |value, i|
+    # summing up values in lower_sum til the sum is >= 3% of the sum of all data values
+    # last_lower_i will be the data position right before lower_sum summed equal to 3% of the sum of all data values
+    if lower_sum < three_percent
+      lower_sum += value
+      last_lower_i = i
+    end
+
+    # // the same with last_upper_i
+    if upper_sum < three_percent
+      upper_sum += histogram[histogram.size - 1 - i]
+      last_upper_i = histogram.size - 1 - i
+    end
+  end
+
+  {
+    min: math_map_value(last_lower_i, 0, 255, min, max),
+    max: math_map_value(last_upper_i, 0, 255, min, max)
+  }
+end
+
+# return image bands depending of band_order
+def bandsplit(image, band_order)
+  if band_order == "GRN"
+    second, first, third, alpha = image.bandsplit
+  else
+    first, second, third, alpha = image.bandsplit
+  end
+  [first, second, third, alpha]
+end
+
+# apply image manipulation algebra
+# VARI
+def vari(image, band_order)
+  r, g, b, alpha = bandsplit(image, band_order)
+  index = (g - r) / (g + r - b)
+  [alpha, index]
+end
+# NDVI
+def ndvi(image, band_order)
+  r, g, nir, alpha = bandsplit(image, band_order)
+  index = (nir - r) / (nir + r)
+  [alpha, index]
+end
+
+def apply_index(base, index)
+  # load image
+  image = Vips::Image.new_from_file("#{base}.png")
+
+  # call index method
+  if index == 'ndvi'
+    alpha, result = ndvi(image, "RGN")
+  elsif index == 'vari'
+    alpha, result = vari(image, "RGB")
+  end
+
+  # it will be used to 'normalize' results
+  histogram = result_histogram(result)
+  clip_min_max = find_clipped_min_max(histogram, result.min, result.max)
+
+  # normalize min/max
+  min = clip_min_max[:min]
+  max = clip_min_max[:max]
+
+  # apply image manipulation algebra to 'normalize' results
+  result = ((result-min) / (max-min).to_f) * 256
+
+  # apply Look-Up-Table (LUT)
+  rdylgn_image = Vips::Image.new_from_array(RdYlGn_lut).bandfold
+  rgb = result.maplut(rdylgn_image)
+
+  # save to file
+  rgb.bandjoin(alpha).write_to_file("#{index}.png")
+end
+
+apply_index('nir', 'ndvi')
+apply_index('rgb', 'vari')
